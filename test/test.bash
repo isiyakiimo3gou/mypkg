b0b6d57ba3de050c1a5edd9baca090821b2ef8c7
hitokoto takumi <takumihekoheko>
2023-12-28 04:09:56 +0900

    a

---
+++ test/test.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep 'Listen: 10'
