99298efaaeabf970237aa29bd4d47e9f120d590c
hitokoto takumi <takumihekoheko>
2024-01-05 01:01:17 +0900

    Update talker.py listener.py

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2023 hitokoto takumi takumihit@icloud.com
 # SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
